7bb8d700417a85ddd441384ab77f265f9e2facf2
r-pustovoy <r.pustovoi@sh.dunice.net>
2025-07-20 20:37:42 +0300

    feat: added initial files for second template

---
+++ template_api_pg_net8_onion/Domain/DTOs/ItemDto.cs
@@ -0,0 +1,12 @@
+namespace customTemlpate.Domain.DTOs;
+
+public class ItemDto
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+}
---
+++ template_api_pg_net8_onion/Domain/Entities/ItemEntity.cs
@@ -0,0 +1,12 @@
+namespace customTemplate.Domain.Entities;
+
+public class ItemEntity
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+}
